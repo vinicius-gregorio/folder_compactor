59b73cf8bfb54f687d96abd42748d09e4f2c542b
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-11-17 09:06:31 -0300

    feat: add compactor package

---
+++ cmd/compactor.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+func CompactFilesToFolder(location string, newLocation string) error {
+	// CreateDirectoryIfNotExists(newLocation)
+	if _, err := os.Stat(newLocation); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(newLocation, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+
+			return err
+		}
+	}
+	f, err := os.Open(location)
+	if err != nil {
+		log.Fatal(err)
+
+		return err
+	}
+	files, err := f.Readdir(0)
+	if err != nil {
+		log.Fatal(err)
+
+		return err
+	}
+
+	for _, v := range files {
+		fmt.Println(v.Name(), v.IsDir())
+		if !v.IsDir() {
+			err := os.Rename(location+"/"+v.Name(), newLocation+"/"+v.Name())
+			if err != nil {
+				log.Fatal(err)
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// func CreateDirectoryIfNotExists(path string) error {
+// 	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+// 		err := os.Mkdir(path, os.ModePerm)
+// 		if err != nil {
+// 			log.Fatal(err)
+
+// 			return err
+// 		}
+// 	}
+
+// 	return nil
+// }
--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"log"
-	"os"
-)
+import "log"
 
 const (
 	LOCATION     = "C:/Users/Gregorio/Downloads/compactor_test"
@@ -14,31 +9,41 @@ const (
 
 func main() {
 
-	f, err := os.Open("C:/Users/Gregorio/Downloads/compactor_test")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	files, err := f.Readdir(0)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// f, err := os.Open("C:/Users/Gregorio/Downloads/compactor_test")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// files, err := f.Readdir(0)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	if _, err := os.Stat(NEW_LOCATION); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir(NEW_LOCATION, os.ModePerm)
-		if err != nil {
-			log.Println(err)
-		}
-	}
+	// if _, err := os.Stat(NEW_LOCATION); errors.Is(err, os.ErrNotExist) {
+	// 	err := os.Mkdir(NEW_LOCATION, os.ModePerm)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// }
 
-	for _, v := range files {
-		fmt.Println(v.Name(), v.IsDir())
-		if !v.IsDir() {
-			err := os.Rename(LOCATION+"/"+v.Name(), NEW_LOCATION+"/"+v.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+	// for _, v := range files {
+	// 	fmt.Println(v.Name(), v.IsDir())
+	// 	if !v.IsDir() {
+	// 		err := os.Rename(LOCATION+"/"+v.Name(), NEW_LOCATION+"/"+v.Name())
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+	// 	}
+	// }
+
+	// err := CreateDirectoryIfNotExists(NEW_LOCATION)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	zz := CompactFilesToFolder(LOCATION, NEW_LOCATION)
+	if zz != nil {
+		log.Fatal(zz)
 	}
 }
