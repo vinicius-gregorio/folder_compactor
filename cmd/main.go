beea736f027deaaec2442e99b6f5e8ea2ac08f9e
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-11-20 16:27:40 -0300

    fix: try setting env

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-
+	os.Setenv("GITHUB_TOKEN", "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")
 	flag.Usage = func() {
 		fmt.Printf("Usage: %s \n\n \t\t go run filesLocation newFilesLocation. \n\n Example: \t go run ./cmd C:/Users/USER/Downloads/compactor_test C:/Users/USER/Downloads/compactor_test/compacted", os.Args[0])
 		flag.PrintDefaults()
